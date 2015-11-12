2973201afa11926fcce8e2ece7b37adef3eb45fa
goku <liushuhuan@bilibili.com>
2015-11-12 19:20:02 +0800

    ch2 begin

---
+++ src/ch2/PrimitiveServlet.java
@@ -0,0 +1,44 @@
+package ch2;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class PrimitiveServlet implements Servlet {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		System.out.println();
+		
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException,
+			IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
